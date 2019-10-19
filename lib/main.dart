036095b3621a00d91394a938847c43d9eee94cb2
EYoung75 <eyoung75.ey@gmail.com>
2019-10-19 00:13:43 -0400

    added custom fonts to assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         accentColor: Colors.deepOrange,
+        fontFamily: "Roboto",
       ),
       routes: {
         "/": (ctx) => TabScreen(),
