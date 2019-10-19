efa2c5aff2f7d86fdfd44c925223ae388a07a497
EYoung75 <eyoung75.ey@gmail.com>
2019-10-18 21:35:07 -0400

    adding google maps setup

---
+++ lib/widgets/map.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class Map extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
