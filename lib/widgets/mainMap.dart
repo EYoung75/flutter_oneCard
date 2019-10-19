910ecf524903d93917bfa14951b5315564ddc6f4
EYoung75 <eyoung75.ey@gmail.com>
2019-10-19 19:06:23 -0400

    added functionality for centering on user location

--- lib/widgets/mainMap.dart
+++ lib/widgets/mainMap.dart
@@ -17,8 +17,13 @@ class MainMapState extends State<MainMap> {
   @override
   void initState() {
     super.initState();
-    geolocator.getCurrentPosition().then((Position position) {
+    userLocation();
+  }
+
+  Future<Position> userLocation() async {
+    await geolocator.getCurrentPosition().then((Position position) {
       setState(() {
+        print(position);
         _currentPosition = position;
       });
     }).catchError(
@@ -28,75 +33,78 @@ class MainMapState extends State<MainMap> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(color: Colors.black38, blurRadius: 40, spreadRadius: 10)
-          ],
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(
-              30,
-            ),
-            bottomRight: Radius.circular(
-              30,
-            ),
-          ),
-        ),
-        height: 300,
-        width: double.infinity,
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(
-              25,
-            ),
-            bottomRight: Radius.circular(
-              25,
-            ),
-          ),
-          child: Stack(
-            children: <Widget>[
-              GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: CameraPosition(
-                  target: LatLng(37.42796133580664, -122.085749655962),
-                  zoom: 14.4746,
-                ),
-                onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
-                },
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(color: Colors.black, width: .5),
-                ),
-                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                height: 35,
-                width: double.infinity,
-                child: TextField(
-                  autocorrect: true,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(5),
-                    labelText: "Search:",
-                    hasFloatingPlaceholder: false,
-                    icon: Icon(
-                      Icons.location_on,
-                      color: Colors.red,
-                    ),
+    return _currentPosition != null
+        ? Center(
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black38, blurRadius: 40, spreadRadius: 10)
+                ],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(
+                    30,
+                  ),
+                  bottomRight: Radius.circular(
+                    30,
                   ),
                 ),
               ),
-              Container(
-                child: Text(
-                    "${_currentPosition.latitude} + ${_currentPosition.longitude}"),
-                color: Colors.red,
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              height: 300,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(
+                    25,
+                  ),
+                  bottomRight: Radius.circular(
+                    25,
+                  ),
+                ),
+                child: Stack(
+                  children: <Widget>[
+                    GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(_currentPosition.latitude,
+                            _currentPosition.longitude),
+                        zoom: 14.4746,
+                      ),
+                      onMapCreated: (GoogleMapController controller) {
+                        _controller.complete(controller);
+                      },
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        border: Border.all(color: Colors.black, width: .5),
+                      ),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      height: 35,
+                      width: double.infinity,
+                      child: TextField(
+                        autocorrect: true,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(5),
+                          labelText: "Search:",
+                          hasFloatingPlaceholder: false,
+                          icon: Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        : Container(
+            margin: EdgeInsets.only(top: 150),
+            child: CircularProgressIndicator(),
+          );
   }
 }
