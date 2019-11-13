4d38da0ff0ffba62ece7dd8912d85c192da2227e
EYoung75 <eyoung75.ey@gmail.com>
2019-11-12 22:59:36 -0600

    added auth card to authscreen and generated auth screen as home route

--- lib/screens/authScreen.dart
+++ lib/screens/authScreen.dart
@@ -26,8 +26,68 @@ class AuthScreen extends StatelessWidget {
           ),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[AuthCard()],
+        ),
+      ),
+    );
+  }
+}
+
+class AuthCard extends StatefulWidget {
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode _authMode = AuthMode.Login;
+  Map<String, String> _authData = {
+    "email": "",
+    "password": "",
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+  AnimationController _controller;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(255, 255, 255, .8),
+        borderRadius: BorderRadius.circular(5)
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      margin: EdgeInsets.all(40),
+      height: 300,
+      width: double.infinity,
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           
+            Text("Sign In"),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'E-Mail'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            RaisedButton(
+              child: Text("Submit"),
+              onPressed: () {},
+            )
           ],
         ),
       ),
