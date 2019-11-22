1f23d4324a2feb24a3eb54f8ef89ee93df639534
EYoung75 <eyoung75.ey@gmail.com>
2019-11-21 21:34:30 -0600

    improved layout of createCard screen

--- lib/screens/createCard.dart
+++ lib/screens/createCard.dart
@@ -24,78 +24,90 @@ class _CreateCardScreenState extends State<CreateCardScreen> {
     // final savedImage = await imageFile.copy("${appDir.path}/${fileName}");
     final filePath = await path.absolute(appDir.toString(), fileName);
     print("FILE PATH: $filePath");
-     setState(() {
+    setState(() {
       _pickedImage = imageFile;
     });
   }
 
- 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context, listen: false);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome to OneCard"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(50),
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).primaryColor.withOpacity(.3),
-              Theme.of(context).primaryColor.withOpacity(.7),
-              Theme.of(context).primaryColor,
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
+    return Container(
+      padding: EdgeInsets.only(left: 30, right: 30),
+      height: double.infinity,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).primaryColor.withOpacity(.3),
+            Theme.of(context).primaryColor.withOpacity(.7),
+            Theme.of(context).primaryColor,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
-        child: Form(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Text("Start by creating your first virtual business card"),
-                Divider(),
-                TextFormField(
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  decoration: InputDecoration(labelText: "Full Name:"),
-                ),
-                TextFormField(
-                  onChanged: (value) {
-                    title = value;
-                  },
-                  decoration: InputDecoration(labelText: "Job Title"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.all(25),
-                  color: Colors.white,
-                  child: _pickedImage != null
-                      ? Image.file(
-                          _pickedImage,
-                          fit: BoxFit.cover,
-                        )
-                      : Text("Select a profile image"),
-                ),
-                FlatButton(
-                  child: Text("Select image"),
-                  onPressed: _selectImage,
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                RaisedButton(
-                  child: Text("Create"),
-                  onPressed: () {
-                    user.createUserProfile(name, title, _pickedImage); 
-                  },
-                )
-              ],
-            ),
+      ),
+      child: Form(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Welcome to OneCard!",
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Start by creating your first virtual business card",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Divider(),
+              TextFormField(
+                onChanged: (value) {
+                  name = value;
+                },
+                decoration: InputDecoration(labelText: "Full Name:"),
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  title = value;
+                },
+                decoration: InputDecoration(labelText: "Job Title"),
+              ),
+              _pickedImage != null
+                  ? Container(
+                      height: 250,
+                      width: 200,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(25),
+                      color: Color.fromRGBO(255, 255, 255, .7),
+                      child: Image.file(
+                        _pickedImage,
+                        fit: BoxFit.cover,
+                      ))
+                  : Container(),
+              FlatButton(
+                child: Text("Select image"),
+                onPressed: _selectImage,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              RaisedButton(
+                child: Text("Create"),
+                onPressed: () {
+                  user.createUserProfile(name, title, _pickedImage);
+                },
+              )
+            ],
           ),
         ),
       ),
